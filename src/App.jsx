801481c1fe5314c398a1a4134f960a579805a731
yehor.baryshnikov <real.horix@gmail.com>
2023-02-16 20:09:19 +0200

    fix issue #7

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { GlobalContextProvider } from './store/GlobalContextProvider';
 import { HomePage } from './components/HomePage/HomePage';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate, HashRouter } from 'react-router-dom';
 import { VariableList } from './components/VariablesList/VariablesList';
 import { VariablesContextProvider } from './store/VariablesContext';
 import { VariableDescription } from './components/VariableDescription/VariableDescription';
@@ -10,14 +10,14 @@ function App() {
   return (
     <GlobalContextProvider>
       <VariablesContextProvider>
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
-            <Route path='*' element={<Navigate replace to={'/vin-decoder'}/>} />
+            <Route path='*' element={<Navigate replace to={'/'}/>} />
             <Route path="/" element={<HomePage/>} />
             <Route path='/variables/' element={<VariableList/>} />
             <Route path='/variables/:id' element={<VariableDescription/>} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </VariablesContextProvider>
     </GlobalContextProvider>
   );
